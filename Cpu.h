684123a25aa70ce43a3faff39e1a61b4dca229e0
Zander <86967831+Zander404@users.noreply.github.com>
2022-04-30 21:40:51 -0300

    - upgrade 01/05/2022

--- Cpu.h
+++ Cpu.h
@@ -8,7 +8,7 @@
 //Codificação: tipo 2
 #define add 0x02
 #define sub 0x03
-#define mult 0x04
+#define mul 0x04
 #define div 0x05
 #define cmp 0x06
 #define movr 0x07
@@ -36,7 +36,7 @@
 #define movi 0x15
 #define addi 0x16
 #define subi 0x17
-#define multi 0x18
+#define muli 0x18
 #define divi 0x19
 #define lsh 0x1a
 #define rsh 0x1b
